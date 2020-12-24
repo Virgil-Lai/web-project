5894401feecf7f0b1f1f18c36a9423a373618164
xie jiaxin <18301111@bjtu.edu.cn>
2020-12-24 16:23:46 +0800

    scroll

--- src/pages/rank-page/RankPage.vue
+++ src/pages/rank-page/RankPage.vue
@@ -9,18 +9,18 @@
         </div>
         <loading v-if="!newMovies.length"></loading>
         <new-rank v-else :movies="newMovies"></new-rank>
-        <div class="load-more" @click="_getMoviesByTag">点击加载更多</div>
+<!--        <div class="load-more" @click="_getMoviesByTag">点击加载更多</div>-->
       </a-col>
       <a-col :span="1"></a-col>
-      <a-col :span="6">
+<!--      <a-col :span="6">-->
 <!--        <loading v-if="!weeklyMovies.length"></loading>-->
 <!--        <movie-rank title="本周口碑榜" :movies="weeklyMovies"></movie-rank>-->
 <!--        <loading v-if="!usMovies.length"></loading>-->
 <!--        <movie-rank v-else title="北美票房榜" :movies="usMovies" :toggle="false"></movie-rank>-->
 <!--        <loading v-if="!topMovies.length"></loading>-->
 <!--        <top-movie v-else :movies="topMovies"></top-movie>-->
-      </a-col>
-      <a-col :span="2"></a-col>
+<!--      </a-col>-->
+<!--      <a-col :span="2"></a-col>-->
     </a-row>
   </div>
 </template>
@@ -49,7 +49,42 @@
         topMovies: []
       }
     },
+    mounted () {
+      //监听鼠标滚动事件
+      // window.addEventListener('mousewheel', this.handleScroll())
+
+    },
     created() {
+      let _this = this;
+      window.onscroll = function(){
+        //变量scrollTop是滚动条滚动时，距离顶部的距离
+        var scrollTop = document.documentElement.scrollTop||document.body.scrollTop;
+        //变量windowHeight是可视区的高度
+        var windowHeight = document.documentElement.clientHeight || document.body.clientHeight;
+        //变量scrollHeight是滚动条的总高度
+        var scrollHeight = document.documentElement.scrollHeight||document.body.scrollHeight;
+        //滚动条到底部的条件
+        if(scrollTop+windowHeight == scrollHeight){
+          //到了这个就可以进行业务逻辑加载后台数据了
+          // alert("hhhhhhhh")
+          // let url='http://localhost:5000/getMovieJsonByTag/?tag='+tag+'&start='+start
+          // console.log(url)
+          getMoviesByUrl(API_WEEKLY).then(res => {
+            res = res.data
+            // this.movies = res.subject
+            // this.movies=[]
+            // this.movies=res.subjects
+            _this.newMovies.push(...res.subjects)
+            // this.movies.append(res.subjects)
+            // console.log(this.newMovies.length)
+            // if(this.newMovies.length>20){
+            //   console.log(movies[22]["title"])
+            // }
+          }).catch(err => {
+            _this.$message.error('获取分类数据失败')
+          })
+        }
+      }
       getMoviesByUrl(API_WEEKLY).then(res => {
         res = res.data
         this.weeklyMovies = res.subjects
@@ -76,11 +111,15 @@
       })
     },
     methods:{
+      // handleScroll(e){
+      //   this.loadMore()
+      // },
       loadMore() {
         // this.start = 0
         this._getMoviesByTag()
       },
       _getMoviesByTag() {
+        // alert("hhhhhhhh")
         // let url='http://localhost:5000/getMovieJsonByTag/?tag='+tag+'&start='+start
         // console.log(url)
         getMoviesByUrl(API_WEEKLY).then(res => {
